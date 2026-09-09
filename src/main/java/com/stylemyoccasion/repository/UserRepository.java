82fc36c69fec209c37259775a9bb8a22c3072a47
sudha409 <sudha409@yahoo.com>
2026-09-08 21:05:33 -0600

    Login & Signup Api -Controller ,Service,model and repository classes

---
+++ src/main/java/com/stylemyoccasion/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.stylemyoccasion.repository;
+
+import com.stylemyoccasion.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}

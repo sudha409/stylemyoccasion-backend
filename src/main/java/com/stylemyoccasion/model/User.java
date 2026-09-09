116f370dc163f785aeb8afaaedb7ca711cff3b00
sudha409 <sudha409@yahoo.com>
2026-09-09 08:31:28 -0600

    formatted files

--- src/main/java/com/stylemyoccasion/model/User.java
+++ src/main/java/com/stylemyoccasion/model/User.java
@@ -1,7 +1,12 @@
 package com.stylemyoccasion.model;
 
-import jakarta.persistence.*;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
 
 @Entity
 @Table(name = "users")

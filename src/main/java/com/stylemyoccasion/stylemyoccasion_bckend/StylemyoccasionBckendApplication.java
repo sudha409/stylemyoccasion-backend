50ac917a8e4a5ab7e44ed19911cb85f025967b8e
sudha409 <sudha409@yahoo.com>
2026-09-08 20:52:51 -0600

    intial project structure

---
+++ src/main/java/com/stylemyoccasion/stylemyoccasion_bckend/StylemyoccasionBckendApplication.java
@@ -0,0 +1,13 @@
+package com.stylemyoccasion.stylemyoccasion_bckend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StylemyoccasionBckendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StylemyoccasionBckendApplication.class, args);
+	}
+
+}

89cb7050a8d2fa210c9f11a095eaab64662411da
sudha409 <sudha409@yahoo.com>
2026-09-13 14:33:37 -0600

    signup changes

--- src/main/java/com/stylemyoccasion/controller/UserController.java
+++ src/main/java/com/stylemyoccasion/controller/UserController.java
@@ -4,10 +4,9 @@ import com.stylemyoccasion.model.User;
 import com.stylemyoccasion.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,6 +19,13 @@ public class UserController {
     @PostMapping("/signup")
     public ResponseEntity<User> signup(@RequestBody User request) {
         User response = userService.signup(request);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @GetMapping("/signup")
+    public ResponseEntity<List<User>> signupDetails() {
+        List<User> response = userService.signupDetails();
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/com/stylemyoccasion/service/UserService.java
+++ src/main/java/com/stylemyoccasion/service/UserService.java
@@ -1,13 +1,26 @@
 package com.stylemyoccasion.service;
 
 import com.stylemyoccasion.model.User;
+import com.stylemyoccasion.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
     public User signup(User request) {
-        return null;
+        return userRepository.save(request);
     }
 
     public User login(User request) {
-        return request;
+        return userRepository.findByEmail(request.getEmail());
+    }
+
+    public List<User> signupDetails() {
+        return userRepository.findAll();
     }
 }
